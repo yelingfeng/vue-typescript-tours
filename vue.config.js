49893b99715a211f083f2f52be723aa2519ecc52
renxiaofan <yelingfeng521@gmail.com>
2019-06-04 12:01:41 +0800

    first commit

---
+++ vue.config.js
@@ -0,0 +1,35 @@
+const BASE_URL = '/Tours/'
+module.exports = {
+    // baseUrl: process.argv[3] === '--dev' ? BASE_URL : '/',
+    publicPath: './',
+    outputDir: 'dist',
+    lintOnSave: true,
+    assetsDir: 'lib',
+    runtimeCompiler: true,
+    productionSourceMap: false,
+    css: {
+        loaderOptions: {
+            css: {
+                localIdentName: '[folder]__[hash:base64:8]',
+                camelCase: 'only'
+            }
+        }
+    },
+    devServer: {
+        port: 8555,
+        https: false,
+        hotOnly: false,
+        proxy: null
+    },
+    configureWebpack: config => {
+        if (process.env.NODE_ENV === 'development') {
+            config.devtool = 'source-map'
+            // mutate config for production...
+        }
+        config.externals = {
+            jquery: 'jQuery',
+            BMap: 'BMap',
+            BMapLib: 'BMapLib'
+        }
+    }
+}
