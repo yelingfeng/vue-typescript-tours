9f8480401a86bb371268d6305dd47aac503e4739
yy406961 <406961038@qq.com>
2019-06-25 15:24:26 +0800

    fix(dashboard.ts): revise a method
    get -> post

--- src/api/dashboard.ts
+++ src/api/dashboard.ts
@@ -35,7 +35,8 @@ export const getBarChartData = () => {
  */
 export const getTrajectoryData = () => {
     return request({
-        url: '/dashboard/routeMock'
+        url: '/dashboard/routeMock',
+        method: 'post'
     })
 }
 /**
